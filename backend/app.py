e9960d12997007de192df340d564980f8f7d28ea
GrampyCat1010 <dubbjg@mail.ru>
2026-07-21 13:44:05 +0400

    Реализовал фронтенд подстраницы Calculator + начали делать бэкенд

--- backend/app.py
+++ backend/app.py
@@ -12,5 +12,13 @@ def hello():
         'message': "Hello From FLask!!!"
     })
 
+@app.route("/api/save_deal")
+def save_deal(name, purchase_price, purchase_date, operation_type, comment):
+    with open("deals.txt", "w", encoding="utf-8") as file:
+        file.write(name, purchase_price, purchase_date, operation_type, comment)
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
